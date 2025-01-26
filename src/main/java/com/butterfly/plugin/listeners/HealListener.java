bf356724b79e7cc23f521160277bc2112429f5e4
Joey61021 <joseph.gardiner58@gmail.com>
2025-01-26 23:07:47 +1000

    Fix null disguise

--- src/main/java/com/butterfly/plugin/listeners/HealListener.java
+++ src/main/java/com/butterfly/plugin/listeners/HealListener.java
@@ -2,7 +2,6 @@ package com.butterfly.plugin.listeners;
 
 import com.butterfly.plugin.managers.PlayerManager;
 import com.butterfly.plugin.utilities.Disguise;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,12 +11,17 @@ public class HealListener implements Listener {
 
     @EventHandler
     public void onHeal(EntityRegainHealthEvent event) {
-        if (!(event.getEntity() instanceof Player) || !PlayerManager.activeDisguises.contains((Player) event.getEntity())) {
+        if (!(event.getEntity() instanceof Player)) {
             return;
         }
 
         Player player = (Player) event.getEntity();
+
         Disguise disguise = PlayerManager.getDisguise(event.getEntity());
+        if (disguise == null) {
+            return;
+        }
+
         disguise.getLivingEntity().setHealth(player.getHealth());
     }
 }
